8dc626be5b7d5742042df403dd6f8ce8e2f8618c
Козлов Андрей <aakozlov85@gmail.com>
2022-08-10 14:08:01 +0300

    скорректирован admin.py и модель Users. Выполнены миграции

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Subscription
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    ordering = ('username',)
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'email',)
+    list_filter = ('username', 'email',)
+
+
+class SubscribtionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author',)
+    search_fields = ('user',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Subscription, SubscribtionAdmin)
--- backend/users/models.py
+++ backend/users/models.py
@@ -5,21 +5,37 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     """Абстрактная модель пользователя."""
-    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
-    email = models.EmailField(verbose_name='email пользователя',
-                              unique=True,
-                              error_messages={
-                                  'unique': "Пользователь с таким email уже существует.",
-                              },
-                              )
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', ]
+    email = models.EmailField(
+        verbose_name='email пользователя',
+        unique=True,
+        error_messages={
+            'unique': "Пользователь с таким email уже существует.",
+        },
+    )
     username = models.CharField(
-        verbose_name='имя пользователя',
+        verbose_name='username пользователя',
         max_length=150,
         unique=True,
         error_messages={
             'unique': "Пользователь с таким именем уже существует.",
         },
     )
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=150,
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
 
 
 class Subscription(models.Model):
