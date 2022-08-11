e46c28f18eb8b23b3d05684bd3eacbde26664622
Козлов Андрей <aakozlov85@gmail.com>
2022-08-11 15:39:53 +0300

    добавлены модели избранного, корзины и связующая модель

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,42 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 
-
-class Recipe(models.Model):
-    """Модель рецепта."""
-
-    tags = models.ManyToManyField(
-        Tag,
-        verbose_name='Тэги',
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор рецепта',
-        related_name='recipes',
-    )
-    ingredients = models.ManyToManyField(
-        Ingredient,
-        verbose_name='Ингредиенты',
-        through='IngredientRecipe',
-    )
-    image = models.ImageField(
-        verbose_name='фото',
-        upload_to='recipes/',
-        blank=True
-    )
-    name = models.CharField(
-        max_length=200,
-        verbose_name='Название',
-    )
-    text = models.TextField(
-        verbose_name='Описание',
-    )
-    cooking_time = models.PositiveIntegerField(
-        verbose_name='Время приготовления',
-        validators=MinValueValidator(1,
-                                     message='Время приготовления не может быть меньше 1 минуты'
-                                     ))
+from users.models import User
 
 
 class Tag(models.Model):
@@ -88,19 +53,111 @@ class Ingredient(models.Model):
         return self.name
 
 
+class Recipe(models.Model):
+    """Модель рецепта."""
+
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тэги',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта',
+        related_name='recipes',
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиенты',
+        through='IngredientRecipe',
+    )
+    image = models.ImageField(
+        verbose_name='фото',
+        upload_to='recipes/',
+        blank=True
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    text = models.TextField(
+        verbose_name='Описание',
+    )
+    cooking_time = models.PositiveIntegerField(
+        verbose_name='Время приготовления',
+        validators=MinValueValidator(
+            1,
+            message='Время приготовления не может быть меньше 1 минуты'
+        )
+    )
+
+    class Meta:
+        ordering = ('-id', )
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
 class IngredientRecipe(models.Model):
     """Связующуя модель количества ингредиентов в рецепте."""
 
-    pass
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField(
+        verbose_name='Количество',
+        validators=MinValueValidator(
+            1,
+            message='Ингредиентов должно быть не менее 1 единицы'
+        )
+    )
+
+    class Meta:
+        verbose_name = 'Количество ингредиента'
+        verbose_name_plural = 'Количество ингредиентов'
 
 
 class Favorite(models.Model):
     """Модель избранных рецептов."""
 
-    pass
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='favorites',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные'
 
 
 class ShoppingCart(models.Model):
     """Модель корзины покупок."""
 
-    pass
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='shopping_cart',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Корзина покупок'
