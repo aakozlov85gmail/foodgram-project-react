a2f73541d04ae8f48fd35b28c11c54890685da55
Козлов Андрей <aakozlov85@gmail.com>
2022-08-21 21:17:31 +0300

    поправил настройки фильтрации

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -17,23 +17,23 @@ class RecipeFilter(filters.FilterSet):
         field_name='tags__slug',
         to_field_name='slug',
     )
-    is_favorited = filters.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='get_is_in_shopping_cart')
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(method='filter_is_in_shopping_cart')
 
-    def get_is_favorited(self, queryset, name, value):
+    def filter_is_favorited(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(
+            return queryset.filter(
                 favorites__user=self.request.user
             )
-        return Recipe.objects.all()
+        return queryset
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
+    def filter_is_in_shopping_cart(self, queryset, name, value):
         if value:
-            return Recipe.objects.filter(
+            return queryset.filter(
                 shopping_cart__user=self.request.user
             )
-        return Recipe.objects.all()
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = ('tags','author',)
+        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart',)
