bc00c3eac192466658fd9fafd1bde881e0ea0f6a
Козлов Андрей <aakozlov85@gmail.com>
2022-08-23 17:28:41 +0300

    настроен docker-compose и nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -63,24 +63,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': os.getenv(key='DB_ENGINE',
-#                             default='django.db.backends.postgresql'),
-#         'NAME': os.getenv(key='DB_NAME', default='postgres'),
-#         'USER': os.getenv(key='POSTGRES_USER', default='postgres'),
-#         'PASSWORD': os.getenv(key='POSTGRES_PASSWORD', default='password'),
-#         'HOST': os.getenv(key='DB_HOST', default='db'),
-#         'PORT': os.getenv(key='DB_PORT', default='5432')
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv(key='DB_ENGINE',
+                            default='django.db.backends.postgresql'),
+        'NAME': os.getenv(key='DB_NAME', default='postgres'),
+        'USER': os.getenv(key='POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv(key='POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv(key='DB_HOST', default='db'),
+        'PORT': os.getenv(key='DB_PORT', default='5432')
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
